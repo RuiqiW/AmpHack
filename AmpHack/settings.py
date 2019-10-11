573e3441d3e4a34653b633d89f3dbf81476dcd28
RuiqiW <37905856+RuiqiW@users.noreply.github.com>
2019-10-11 14:57:38 -0400

    Update settings.py

--- AmpHack/settings.py
+++ AmpHack/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4@78#s+l#_l9a@t!5$!0#%jwt@nrgn+w*j07y+hawik&gja9_7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,18 +72,10 @@ WSGI_APPLICATION = 'AmpHack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'AmpHack',  # as named on server
-        'HOST': 'mongodb+srv://admin:admin@cluster0-tcjyi.mongodb.net/test?retryWrites=true&w=majority',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
